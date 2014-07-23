aa1d6fe0d836e8d4e76958f656714339ae5f6404
Patrick Arlt <patrick.arlt@gmail.com>
2014-07-23 15:11:36 -0700

    don't append array data when using metadata

--- press.conf.js
+++ press.conf.js
@@ -5,20 +5,17 @@ function config(press) {
 
   press.global('rootUrl', 'http://site.com');
 
-  press.metadata('page-with-external-data*', {
-    data: ['moarData']
-  });
-
   press.ignore('ignore-me.html');
 
   press.collection('blog', 'posts/**/*');
 
   press.layout('**/*', 'layouts/_layout:content');
   press.layout('posts/**/*', 'layouts/_post:post');
+
 }
 
 config.options = {
   src: 'src'
-}
+};
 
 module.exports = config;
