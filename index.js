24762b99d29446b8c898cffd0ce053df3d31d9d5
Patrick Arlt <patrick.arlt@gmail.com>
2015-04-25 18:06:45 -0700

    wrap up refactor

--- index.js
+++ index.js
@@ -1,34 +1,89 @@
-var Acetate = require('./lib/acetate');
+var _ = require('lodash');
+var EventEmitter = require('events').EventEmitter;
+var utils = require('./lib/utils');
 
-module.exports = function (options) {
-  var site = new Acetate().init(options);
+var logger = require('./lib/mixins/logger');
+var nunjucks = require('./lib/mixins/nunjucks');
+var markdown = require('./lib/mixins/markdown');
+var loader = require('./lib/mixins/loader');
+var builder = require('./lib/mixins/builder');
+var helpers = require('./lib/mixins/helpers');
+var server = require('./lib/mixins/server');
+var watcher = require('./lib/mixins/watcher');
+var initializer = require('./lib/mixins/initializer');
 
-  function postBuild () {
-    if (options.server || options.watcher) {
-      site.watcher.start();
-    }
+function acetate (options) {
+  // merge options with defaults
+  options = _.defaults(options, {
+    config: 'acetate.conf.js',
+    src: 'src',
+    dest: 'build',
+    root: process.cwd(),
+    log: 'info',
+    watcher: false,
+    server: false,
+    host: 'localhost',
+    port: 8000,
+    findPort: false,
+    args: {}
+  });
 
-    if (options.server) {
-      site.server.start(options);
-    }
-  }
+  // create the `site` object which is events + logger + options + utils
+  var site = _.extend({}, EventEmitter.prototype, logger(), {
+    util: utils(options),
+    options: Object.freeze(options)
+  });
 
-  site.log.time('load');
+  // define read-only `root` property on `site`
+  Object.defineProperty(site, 'root', {
+    value: options.root,
+    enumerable: true
+  });
 
-  site.load(function () {
-    if (options.clean) {
-      site.log.time('clean');
-      site.clean(function (error) {
-        if (error) {
-          site.log.error('cleaning', 'error cleaning old pages %s', error);
-        }
-        site.log.verbose('cleaning', 'cleaning build folder done in %s', site.log.timeEnd('clean'));
-        site.build(postBuild);
-      });
-    } else {
-      site.build(postBuild);
-    }
+  // define read-only `src` property on `site`
+  Object.defineProperty(site, 'src', {
+    value: options.src,
+    enumerable: true
+  });
+
+  // define read-only `dest` property on `site`
+  Object.defineProperty(site, 'dest', {
+    value: options.dest,
+    enumerable: true
+  });
+
+  // define read-only `config` property on `site`
+  Object.defineProperty(site, 'config', {
+    value: options.config,
+    enumerable: true
+  });
+
+  // define read-only `args` on `site`
+  Object.defineProperty(site, 'args', {
+    value: Object.freeze(options.args),
+    enumerable: true
+  });
+
+  // mixin all of the different components
+  // do each seperatly so they are cumulative
+  // some have some dependencies on each other
+  _.extend(site, markdown(site));
+  _.extend(site, nunjucks(site));
+  _.extend(site, loader(site));
+  _.extend(site, builder(site));
+  _.extend(site, helpers(site));
+  _.extend(site, server(site));
+  _.extend(site, watcher(site));
+  _.extend(site, initializer(site));
+
+  // on the next run of the event loop start everything
+  // this delays initalziation so event listeners can be added
+  // https://nodejs.org/api/process.html#process_process_nexttick_callback
+  process.nextTick(function () {
+    site.init();
   });
 
   return site;
 };
+
+module.exports = acetate;
--- test/features/template-errors/test.js
+++ test/features/template-errors/test.js
@@ -1,131 +1,134 @@
 var test = require('tape');
 var utils = require('../utils');
-var acetate = require('../../../index.js');
 var _ = require('lodash');
 
 var root = __dirname;
 
-var site = acetate({
+utils.start({
   log: 'silent',
-  root: root,
-  clean: true
-});
+  root: root
+}, function (error, site) {
+  if (error) {
+    console.log(error);
+    process.exit(1);
+  }
 
-var logs = [];
+  var logs = [];
 
-site.on('log', function (e) {
-  logs.push(e);
-});
-
-site.once('clean', function () {
-  logs = [];
-});
-
-site.once('build', function () {
-  test('should render a page with multiple metadata blocks properly', function (t) {
-    t.plan(1);
-
-    var output = 'build/metadata-metadata/index.html';
-    var expected = 'expected/metadata-metadata.html';
-
-    utils.equal(t, root, output, expected);
+  site.on('log', function (e) {
+    logs.push(e);
   });
 
-  test('should log an error when Nunjucks throws an error in a template with metadata', function (t) {
-    t.plan(1);
-
-    var expected = {
-      level: 'error',
-      category: 'page',
-      text: 'error building template-error-with-metadata.html - unknown block tag: undefined - src/template-error-with-metadata.html:5:2'
-    };
-
-    var log = _.where(logs, expected)[0];
-
-    t.deepEqual(log, expected);
+  site.once('clean', function () {
+    logs = [];
   });
 
-  test('should log an error when Nunjucks throws an error in a template no metadata', function (t) {
-    t.plan(1);
+  site.once('build', function () {
+    test('should render a page with multiple metadata blocks properly', function (t) {
+      t.plan(1);
 
-    var expected = {
-      level: 'error',
-      category: 'page',
-      text: 'error building template-error-no-metadata.html - unknown block tag: undefined - src/template-error-no-metadata.html:1:2'
-    };
+      var output = 'build/metadata-metadata/index.html';
+      var expected = 'expected/metadata-metadata.html';
 
-    var log = _.where(logs, expected)[0];
+      utils.equal(t, root, output, expected);
+    });
 
-    t.deepEqual(log, expected);
-  });
+    test('should log an error when Nunjucks throws an error in a template with metadata', function (t) {
+      t.plan(1);
 
-  test('should log an error when Nunjucks throws an error in a template leading newlines', function (t) {
-    t.plan(1);
+      var expected = {
+        level: 'error',
+        category: 'page',
+        text: 'error building template-error-with-metadata.html - unknown block tag: undefined - src/template-error-with-metadata.html:5:2'
+      };
 
-    var expected = {
-      level: 'error',
-      category: 'page',
-      text: 'error building template-error-leading-newlines.html - unknown block tag: undefined - src/template-error-leading-newlines.html:5:2'
-    };
+      var log = _.where(logs, expected)[0];
 
-    var log = _.where(logs, expected)[0];
+      t.deepEqual(log, expected);
+    });
 
-    t.deepEqual(log, expected);
-  });
+    test('should log an error when Nunjucks throws an error in a template no metadata', function (t) {
+      t.plan(1);
 
-  test('should log an error when a helper hander throws an error', function (t) {
-    t.plan(1);
+      var expected = {
+        level: 'error',
+        category: 'page',
+        text: 'error building template-error-no-metadata.html - unknown block tag: undefined - src/template-error-no-metadata.html:1:2'
+      };
 
-    var expected = {
-      level: 'error',
-      category: 'page',
-      text: 'error building helper-error.html - error in custom helper "errorHelper" - acetate.conf.js:3:11'
-    };
+      var log = _.where(logs, expected)[0];
 
-    var log = _.where(logs, expected)[0];
+      t.deepEqual(log, expected);
+    });
 
-    t.deepEqual(log, expected);
-  });
+    test('should log an error when Nunjucks throws an error in a template leading newlines', function (t) {
+      t.plan(1);
 
-  test('should log an error when a helper is called with no params', function (t) {
-    t.plan(1);
+      var expected = {
+        level: 'error',
+        category: 'page',
+        text: 'error building template-error-leading-newlines.html - unknown block tag: undefined - src/template-error-leading-newlines.html:5:2'
+      };
 
-    var expected = {
-      level: 'error',
-      category: 'page',
-      text: 'error building helper-no-params.html - You must pass at least one parameter to custom helper "helper"'
-    };
+      var log = _.where(logs, expected)[0];
 
-    var log = _.where(logs, expected)[0];
+      t.deepEqual(log, expected);
+    });
 
-    t.deepEqual(log, expected);
-  });
+    test('should log an error when a helper hander throws an error', function (t) {
+      t.plan(1);
 
-  test('should log an error when a filter hander throws an error', function (t) {
-    t.plan(1);
+      var expected = {
+        level: 'error',
+        category: 'page',
+        text: 'error building helper-error.html - error in custom helper "errorHelper" - acetate.conf.js:3:11'
+      };
 
-    var expected = {
-      level: 'error',
-      category: 'page',
-      text: 'error building filter-error.html - error in custom filter "errorFilter" - acetate.conf.js:11:11'
-    };
+      var log = _.where(logs, expected)[0];
 
-    var log = _.where(logs, expected)[0];
+      t.deepEqual(log, expected);
+    });
 
-    t.deepEqual(log, expected);
-  });
+    test('should log an error when a helper is called with no params', function (t) {
+      t.plan(1);
 
-  test('should log an error when a block hander throws an error', function (t) {
-    t.plan(1);
+      var expected = {
+        level: 'error',
+        category: 'page',
+        text: 'error building helper-no-params.html - You must pass at least one parameter to custom helper "helper"'
+      };
 
-    var expected = {
-      level: 'error',
-      category: 'page',
-      text: 'error building block-error.html - error in custom block "errorBlock" - acetate.conf.js:7:11'
-    };
+      var log = _.where(logs, expected)[0];
 
-    var log = _.where(logs, expected)[0];
+      t.deepEqual(log, expected);
+    });
 
-    t.deepEqual(log, expected);
+    test('should log an error when a filter hander throws an error', function (t) {
+      t.plan(1);
+
+      var expected = {
+        level: 'error',
+        category: 'page',
+        text: 'error building filter-error.html - error in custom filter "errorFilter" - acetate.conf.js:11:11'
+      };
+
+      var log = _.where(logs, expected)[0];
+
+      t.deepEqual(log, expected);
+    });
+
+    test('should log an error when a block hander throws an error', function (t) {
+      t.plan(1);
+
+      var expected = {
+        level: 'error',
+        category: 'page',
+        text: 'error building block-error.html - error in custom block "errorBlock" - acetate.conf.js:7:11'
+      };
+
+      var log = _.where(logs, expected)[0];
+
+      t.deepEqual(log, expected);
+    });
   });
 });
