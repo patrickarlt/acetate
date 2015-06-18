f86ec41a2415315bfba2bb64b826951c38a38a08
Patrick Arlt <patrick.arlt@gmail.com>
2015-06-18 11:30:44 -0700

    refactor tests

--- test/watcher/test.js
+++ test/watcher/test.js
@@ -6,12 +6,12 @@ var path = require('path');
 var root = __dirname;
 
 utils.start({
-  log: 'silent',
+  log: 'debug',
   watcher: true,
   root: root
 }, function (site) {
   var filepath = path.join(__dirname, site.src, 'index.html');
-
+  console.log(filepath);
   test('should build a file when an added event is fired', function (t) {
     t.plan(1);
     t.timeoutAfter(3000);
@@ -23,34 +23,34 @@ utils.start({
           var expected = path.join('expected', 'index-added.html');
 
           utils.equal(t, root, output, expected);
+
+          test('should re-build a file when a changed event is fired', function (t) {
+            t.plan(1);
+            t.timeoutAfter(6000);
+
+            fs.appendFile(filepath, '\nchanged', function () {
+              site.once('build', function () {
+                var output = path.join('build', 'index.html');
+                var expected = path.join('expected', 'index-changed.html');
+
+                utils.equal(t, root, output, expected);
+
+                test('should remove a file when an unlink event is fired', function (t) {
+                  t.plan(2);
+                  t.timeoutAfter(12000);
+
+                  fs.unlink(filepath, function () {
+                    site.once('page:clean', function () {
+                      t.notOk(site.util.exists(path.join(__dirname, site.dest, 'index.html')));
+                      t.equal(site.pages.length, 0, 'page should be removed from pages array');
+                      site.stopWatcher();
+                    });
+                  });
+                });
+              });
+            });
+          });
         });
-      });
-    });
-  });
-
-  test('should re-build a file when a changed event is fired', function (t) {
-    t.plan(1);
-    t.timeoutAfter(3000);
-
-    fs.appendFile(filepath, '\nchanged', function () {
-      site.once('build', function () {
-        var output = path.join('build', 'index.html');
-        var expected = path.join('expected', 'index-changed.html');
-
-        utils.equal(t, root, output, expected);
-      });
-    });
-  });
-
-  test('should remove a file when an unlink event is fired', function (t) {
-    t.plan(2);
-    t.timeoutAfter(3000);
-
-    fs.unlink(filepath, function () {
-      site.once('page:clean', function () {
-        t.notOk(site.util.exists(path.join(__dirname, site.dest, 'index.html')));
-        t.equal(site.pages.length, 0, 'page should be removed from pages array');
-        site.stopWatcher();
       });
     });
   });
