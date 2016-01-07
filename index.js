d67b61c8fff095acb0058b881959ea742e47bc2a
john gravois <jgravois@esri.com>
2016-01-07 15:37:51 -0800

    spellcheck

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ module.exports = function (options, callback) {
   }
 
   function pageBuilder (request, response, next) {
-    site.info('server', 'request recived for %s', request.url);
+    site.info('server', 'request received for %s', request.url);
 
     if (request.method !== 'GET') {
       next();
